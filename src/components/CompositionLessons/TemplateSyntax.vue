e0e24ca9ff145fdba653f1d16adeb51a490fb105
hkjj293 <chrishuihuihui@gmail.com>
2023-03-05 16:00:51 +0000

    Update TempSyn

--- src/components/CompositionLessons/TemplateSyntax.vue
+++ src/components/CompositionLessons/TemplateSyntax.vue
@@ -11,7 +11,10 @@ const objectOfAttrs = {
     class: 'wrapper'
 }
 var seen = true;
-const href = "https://chrishui.cc";
+const url = "https://chrishui.cc";
+const attributeName = "href";
+const eventName = "click"
+
 function add(a, b) {
     return a + b;
 }
@@ -39,12 +42,20 @@ function doSomething() {
     <!-- Directives -->
     <p v-if="seen">Now you see me (Directives)</p>
     <!-- Arguments href is the argument-->
-    <a v-bind:href="url">Link</a>
-    <a v-on:click="doSomething"> ... </a>
-    <a @click="doSomething"> ... </a>
+    <a href="https://chrishui.cc">Link</a><br />
+    <a v-bind:href="url">Link</a><br /> <!-- Equal to <a v-bind="attr">Link</a>  ;   attr = {href: url} -->
+    <a href="" v-on:click="doSomething"> v-on:click </a><br />
+    <a href="" @click="doSomething"> @click </a><br /> <!-- shorthand -->
+    <!-- Dynamic Arguments -->
+    <a v-bind:[attributeName]="url"> v-bind:[attributeName] </a><br />
+    <a href="" v-on:[eventName]="doSomething"> v-on:[eventName] </a>
 </template>
 
 <style scoped>
+a {
+    color: lightgreen;
+}
+
 button {
     font-weight: bold;
 }
