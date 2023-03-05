18eba4239a0b99dd689ca7918986d807b3cd1e20
hkjj293 <chrishuihuihui@gmail.com>
2023-03-05 16:25:54 +0000

    Completed TempSyn

--- src/components/CompositionLessons/TemplateSyntax.vue
+++ src/components/CompositionLessons/TemplateSyntax.vue
@@ -21,34 +21,46 @@ function add(a, b) {
 function doSomething() {
     seen = !seen;
 }
+function onSubmit(event) {
+
+}
 </script>
 
 <template>
-    <br />
-    <!-- Text Interpolation -->
-    <span>Message: {{ msg }}</span>
-    <!-- Raw HTML -->
-    <p>Text Interp: {{ rawHtml }}</P>
-    <p>Raw HTML v-html: <span v-html="rawHtml"></span></p>
-    <!-- Attribute Bindings -->
-    <div v-bind:id="dynamicId">dynamicId</div>
-    <div :id="dynamicId">dynamicId</div> <!-- shorthand -->
-    <!-- Boolean attribute -->
-    <button :disabled="disableButton">Button</button>
-    <!-- Dynamic Binding multiple sttribute -->
-    <div v-bind="objectOfAttrs">objectOfAttrs</div>
-    <!-- Use js expressions (not statments)-->
-    {{ add(1, 1) + " (Example of js expression)" }}
-    <!-- Directives -->
-    <p v-if="seen">Now you see me (Directives)</p>
-    <!-- Arguments href is the argument-->
-    <a href="https://chrishui.cc">Link</a><br />
-    <a v-bind:href="url">Link</a><br /> <!-- Equal to <a v-bind="attr">Link</a>  ;   attr = {href: url} -->
-    <a href="" v-on:click="doSomething"> v-on:click </a><br />
-    <a href="" @click="doSomething"> @click </a><br /> <!-- shorthand -->
-    <!-- Dynamic Arguments -->
-    <a v-bind:[attributeName]="url"> v-bind:[attributeName] </a><br />
-    <a href="" v-on:[eventName]="doSomething"> v-on:[eventName] </a>
+    <div>
+        <br />
+        <!-- Text Interpolation -->
+        <span>Message: {{ msg }}</span>
+        <!-- Raw HTML -->
+        <p>Text Interp: {{ rawHtml }}</P>
+        <p>Raw HTML v-html: <span v-html="rawHtml"></span></p>
+        <!-- Attribute Bindings -->
+        <div v-bind:id="dynamicId">dynamicId</div>
+        <div :id="dynamicId">dynamicId</div> <!-- shorthand -->
+        <!-- Boolean attribute -->
+        <button :disabled="disableButton">Button</button>
+        <!-- Dynamic Binding multiple sttribute -->
+        <div v-bind="objectOfAttrs">objectOfAttrs</div>
+        <!-- Use js expressions (not statments)-->
+        {{ add(1, 1) + " (Example of js expression)" }}
+        <!-- Directives -->
+        <p v-if="seen">Now you see me (Directives)</p>
+        <!-- Arguments href is the argument-->
+        <a href="https://chrishui.cc">Link</a><br />
+        <a v-bind:href="url">Link</a><br /> <!-- Equal to <a v-bind="attr">Link</a>  ;   attr = {href: url} -->
+        <a href="" v-on:click="doSomething"> v-on:click </a><br />
+        <a href="" @click="doSomething"> @click </a><br /> <!-- shorthand -->
+        <!-- Dynamic Arguments -->
+        <a v-bind:[attributeName]="url"> v-bind:[attributeName] </a><br />
+        <a href="" v-on:[eventName]="doSomething"> v-on:[eventName] </a><br />
+        <!-- DA constraints -->
+        <!-- <a :['foo' + bar]="value"> ... </a> !!Error -->
+        <!-- <a :[someAttr]="value"> ... </a> !!someAttr will become someattr when not in SFC-->
+        <!-- Modifiers -->
+        <form @submit.prevent="onSubmit">
+            <button type="submit">Submit</button>
+        </form>
+    </div>
 </template>
 
 <style scoped>
