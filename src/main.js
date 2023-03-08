4fb957d41241d89c78f6be6e77e831956f4eaf8b
hkjj293 <chrishuihuihui@gmail.com>
2023-03-08 14:30:50 +0000

    update

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import * as components from './components/Imports'
 
 const app = createApp(App);
 Object.entries(components).forEach(([compName, comp]) => {
-    app.component(comp.name || compName, comp);
-  });
+  app.component(comp.name || compName, comp);
+});
 app.mount('#app');
 // From Creating an Application
 app.config.errorHandler = (err) => {
-   /* handle error */
+  /* handle error */
 }
+
+
